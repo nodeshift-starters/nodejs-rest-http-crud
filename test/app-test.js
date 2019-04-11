fac33ee16c8ca73cc98fbcc194fb026fa55f7981
Philip Hayes <phayes@redhat.com>
2019-04-11 15:42:58 -0400

    Added tests for probe in app.js

---
+++ test/app-test.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const test = require("tape");
+const supertest = require("supertest");
+const proxyquire = require("proxyquire");
+
+
+
+test("test liveness ok", t => {
+  const mockDb = {
+    init: () => {
+      return Promise.resolve();
+    },
+    query: (query, cb) => {
+      cb(null, true)
+    }
+  };
+
+  const app = proxyquire("../app", {
+    './lib/db': mockDb,
+  });
+
+  supertest(app)
+    .get("/api/health/liveness")
+    .expect(200)
+    .then(response => {
+      t.equal(response.text, 'OK', "should return OK");
+      t.end();
+    });
+});
+
+test("test liveness not ok", t => {
+  const mockDb = {
+    init: () => {
+      return Promise.resolve();
+    },
+    query: (query, cb) => {
+      cb('error')
+    }
+  };
+  
+  const app = proxyquire("../app", {
+    './lib/db': mockDb,
+  });
+
+  supertest(app)
+    .get("/api/health/liveness")
+    .expect(500)
+    .then(response => {
+      t.equal(response.text, 'not ok', "should return not ok");
+      t.end();
+    });
+});
