dbe4bb189800a433babf2278e0f62f411bd2fa2c
Philip Hayes <phayes@redhat.com>
2019-04-11 13:08:02 -0400

    replaced double quotes

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-"use strict";
+'use strict';
 
 /*
  *
  *  Copyright 2016-2017 Red Hat, Inc, and individual contributors.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  Licensed under the Apache License, Version 2.0 (the 'License');
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
  *  http://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  distributed under the License is distributed on an 'AS IS' BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
  */
 
-const path = require("path");
-const express = require("express");
-const bodyParser = require("body-parser");
-const swaggerUi = require("swagger-ui-express");
-const swaggerJSDoc = require("swagger-jsdoc");
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
 const app = express();
-const probe = require("kube-probe");
-const db = require("./lib/db");
+const probe = require('kube-probe');
+const db = require('./lib/db');
 
 let livenessCallback = (req, res) => {
-  db.query("select now()", err => {
+  db.query('select now()', err => {
     if (!err) {
       res.writeHead(200);
-      res.end("OK");
+      res.end('OK');
     } else {
-      console.log("liveness not ok");
+      console.log('liveness not ok');
       res.writeHead(500);
-      res.end("not ok");
+      res.end('not ok');
     }
   });
 };
@@ -45,11 +45,11 @@ const probeOptions = {
 var swaggerDefinition = {
   info: {
     // API informations
-    title: "Fruits", 
-    version: "2.1.1", 
-    description: "A sample RESTful API" 
+    title: 'Fruits', 
+    version: '2.1.1', 
+    description: 'A sample RESTful API' 
   },
-  basePath: "/" 
+  basePath: '/' 
 };
 
 // Options for the swagger docs
@@ -57,50 +57,50 @@ var options = {
   // Import swaggerDefinitions
   swaggerDefinition: swaggerDefinition,
   // Path to the API docs
-  apis: ["./lib/routes/fruits.js"]
+  apis: ['./lib/routes/fruits.js']
 };
 const swaggerSpec = swaggerJSDoc(options);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-const fruits = require("./lib/routes/fruits");
+const fruits = require('./lib/routes/fruits');
 
 app.use(bodyParser.json());
 app.use((error, req, res, next) => {
   if (
-    req.body === "" ||
-    (error instanceof SyntaxError && error.type === "entity.parse.failed")
+    req.body === '' ||
+    (error instanceof SyntaxError && error.type === 'entity.parse.failed')
   ) {
     res.status(415);
-    return res.send("Invalid payload!");
+    return res.send('Invalid payload!');
   }
 
   next();
 });
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 // Expose the license.html at http[s]://[host]:[port]/licences/licenses.html
-app.use("/licenses", express.static(path.join(__dirname, "licenses")));
+app.use('/licenses', express.static(path.join(__dirname, 'licenses')));
 
-app.use("/api", fruits);
+app.use('/api', fruits);
 
 // Add a health check
 
 db.init()
   .then(() => {
-    console.log("Database init'd, starting probe");
+    console.log('Database init\'d, starting probe');
     probe(app, probeOptions);
   })
   .catch(error => {
     console.log(error);
   });
 
-process.on("SIGTERM", function onSigterm() {
+process.on('SIGTERM', function onSigterm() {
   console.info(
-    "Got SIGTERM. Graceful shutdown start now",
+    'Got SIGTERM. Graceful shutdown start now',
     new Date().toISOString()
   );
   db.end();
-  console.info("DB Shutdown");
+  console.info('DB Shutdown');
 });
 
 module.exports = app;
